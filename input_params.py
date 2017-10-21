a2e6a73b3abad45458894b6488dff013b82fe98b
Unknown <s.sanchezperezmoreno@tudelft.nl>
2017-10-21 23:32:17 +0200

    save

--- input_params.py
+++ input_params.py
@@ -1,4 +1,4 @@
-n_turbines = 5
+n_turbines = 3
 u_far = 8.5  # Freestream wind velocity
 jensen_k = 0.04
 turbine_radius = 40.0
--- workflow.py
+++ workflow.py
@@ -9,7 +9,7 @@ class WorkingGroup(Group):
     def setup(self):
         indep2 = self.add_subsystem('indep2', IndepVarComp())
         # indep2.add_output('layout', val=read_layout('horns_rev9.dat'))
-        indep2.add_output('layout', val=np.array([[0, 0.0, 0.0], [1, 560.0, 560.0], [2, 1120.0, 1120.0], [3, 1120.0, 0.0], [4, 0.0, 1120.0]]))
+        indep2.add_output('layout', val=np.array([[0, 0.0, 0.0], [1, 560.0, 0.0], [2, 1120.0, 0.0]]))
         indep2.add_output('angle', val=180.0)
         indep2.add_output('r', val=turbine_radius)
         indep2.add_output('k', val=jensen_k)
