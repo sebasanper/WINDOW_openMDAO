e067b8493d386f6bb84ce231d9f6edff9eafd227
Sebastian <s.sanchezperezmoreno@tudelft.nl>
2017-12-01 14:33:18 -0700

    three phase current right

--- input_params.py
+++ input_params.py
@@ -12,7 +12,7 @@ areas = np.array([[[0, 0], [1120, 0.0], [1120.0, 1120.0], [0.0, 1120.0]]])
 # Turbine parameters
 turbine_rated_power = 2000000.0
 turbine_voltage = 33000.0
-turbine_rated_current = sqrt(3.0) * turbine_rated_power / (turbine_voltage)  # A = Power * sqrt(3) / Voltage. 3 phase current per line.
+turbine_rated_current = turbine_rated_power / (turbine_voltage  * sqrt(3.0))  # A = Power / sqrt(3) / Voltage. 3 phase current per line.
 
 cable_types = [[95, 300, 206], [120, 340, 221], [150, 375, 236], [185, 420, 256], [240, 480, 287], [300, 530, 316], [400, 590, 356], [500, 655, 406], [630, 715, 459], [800, 775, 521], [1000, 825, 579]] # Current, cross section, cost
 
