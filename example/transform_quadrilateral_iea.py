19e373e30a2586d0b6b427fc0dce676b8d9e4b67
sebasanper <s.sanchezperezmoreno@tudelft.nl>
2018-01-26 13:08:56 +0100

    save

--- example/transform_quadrilateral_iea.py
+++ example/transform_quadrilateral_iea.py
@@ -3,6 +3,7 @@ from numpy.linalg import solve, inv
 
 
 class AreaMapping:
+    # Maps any quadrilateral (real coordinates) to another specified rectangle (desired coordinates)
     def __init__(self, real_area_coords, desired_rectangle_coords):
         self.real_area_coords = real_area_coords
         self.desired_rectangle_coords = desired_rectangle_coords
@@ -41,66 +42,3 @@ class AreaMapping:
     def transform_to_shape(self, u, v):
         non_dim = matmul(self.map_to_shape, [u, v, 1])
         return non_dim[0] / non_dim[2], non_dim[1] / non_dim[2]
-
-
-if __name__ == '__main__':
-    area_real_coordinates = [[200, 100], [400, 150], [100, 300], [500, 400]]
-    # area_real_coordinates = [[100, 100], [500, 100], [300, 500], [301, 501]]
-    desired_rectangle_coordinates = [[0, 0], [1, 0], [0, 1], [1, 1]]
-    n = 1000.0
-
-    mapping1 = AreaMapping(area_real_coordinates, desired_rectangle_coordinates)
-
-    with open("points.dat", "w") as out:
-        xx = area_real_coordinates
-        out.write("{} {} {} {} {} {} {} {}".format(xx[0][0], xx[0][1], xx[1][0], xx[1][1], xx[2][0], xx[2][1], xx[3][0], xx[3][1]))
-        for i in range(int(n)):
-            point_old_x = xx[0][0] + (xx[1][0] - xx[0][0]) / n * i
-            point_old_y = xx[0][1] + (xx[1][1] - xx[0][1]) / n * i
-            new_x, new_y = mapping1.transform_to_rectangle(point_old_x, point_old_y)
-            out.write("\n{} {} {} {}".format(point_old_x, point_old_y, new_x, new_y))
-        for i in range(int(n)):
-            old_x = xx[1][0] + (xx[3][0] - xx[1][0]) / n * i
-            old_y = xx[1][1] + (xx[3][1] - xx[1][1]) / n * i
-            new_x, new_y = mapping1.transform_to_rectangle(old_x, old_y)
-            out.write("\n{} {} {} {}".format(old_x, old_y, new_x, new_y))
-        for i in range(int(n)):
-            old_x = xx[3][0] + (xx[2][0] - xx[3][0]) / n * i
-            old_y = xx[3][1] + (xx[2][1] - xx[3][1]) / n * i
-            new_x, new_y = mapping1.transform_to_rectangle(old_x, old_y)
-            out.write("\n{} {} {} {}".format(old_x, old_y, new_x, new_y))
-        for i in range(int(n)):
-            old_x = xx[2][0] + (xx[0][0] - xx[2][0]) / n * i
-            old_y = xx[2][1] + (xx[0][1] - xx[2][1]) / n * i
-            new_x, new_y = mapping1.transform_to_rectangle(old_x, old_y)
-            out.write("\n{} {} {} {}".format(old_x, old_y, new_x, new_y))
-        # for i in range(n):
-        #     old_x = 500 - .3 * i
-        #     old_y = 400 - .3 * i
-        #     new_x, new_y = transform_to_rectangle(old_x, old_y)
-        #     out.write("\n{} {} {} {}".format(old_x, old_y, new_x, new_y))
-        # for i in range(n):
-        #     old_x = n + .3 * i
-        #     old_y = 300 - .15 * i
-        #     new_x, new_y = transform_to_rectangle(old_x, old_y)
-        #     out.write("\n{} {} {} {}".format(old_x, old_y, new_x, new_y))
-        for i in range(int(n)):
-            new_x = 0.5 + 0.2 * cos(pi / n * i)
-            new_y = 0.5 + 0.2 * sin(pi / n * i)
-            old_x, old_y = mapping1.transform_to_shape(new_x, new_y)
-            out.write("\n{} {} {} {}".format(old_x, old_y, new_x, new_y))
-        for i in range(int(n)):
-            new_x = 0.5 - 0.2 * cos(pi / n * i)
-            new_y = 0.5 - 0.2 * sin(pi / n * i)
-            old_x, old_y = mapping1.transform_to_shape(new_x, new_y)
-            out.write("\n{} {} {} {}".format(old_x, old_y, new_x, new_y))
-        for i in range(int(n)):
-            old_x = 300 + 50.0 * cos(pi / n * i)
-            old_y = 200 + 50.0 * sin(pi / n * i)
-            new_x, new_y = mapping1.transform_to_rectangle(old_x, old_y)
-            out.write("\n{} {} {} {}".format(old_x, old_y, new_x, new_y))
-        for i in range(int(n)):
-            old_x = 300 - 50.0 * cos(pi / n * i)
-            old_y = 200 - 50.0 * sin(pi / n * i)
-            new_x, new_y = mapping1.transform_to_rectangle(old_x, old_y)
-            out.write("\n{} {} {} {}".format(old_x, old_y, new_x, new_y))
