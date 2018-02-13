6c7d62a2fcbc0f21f421aa6d42c946bed19a89e9
Erik Quaeghebeur <E.R.G.Quaeghebeur@tudelft.nl>
2018-02-13 14:50:32 +0100

    expand setup file with some metadata and a partial package list

--- setup.py
+++ setup.py
@@ -2,8 +2,35 @@ from distutils.core import setup
 
 setup(
     name='WINDOW_openMDAO',
-    #packages=[],
-    install_requires=[
-        'openmdao'
+    version='',
+    description="WINDOW is an MDAO workflow to design offshore wind farms",
+    long_description="""
+    WINDOW is an MDAO workflow to design offshore wind farms. It is written in
+    NASA's openMDAO framework. The following publication shows the Extended
+    Design Structure Matrix of WINDOW_openMDAO:
+
+    Sebastian Sanchez Perez Moreno and Michiel B. Zaaijer. “How to select MDAO workflows”,
+    2018 AIAA/ASCE/AHS/ASC Structures, Structural Dynamics, and Materials Conference,
+    AIAA SciTech Forum, (AIAA 2018-0654) https://doi.org/10.2514/6.2018-0654
+    """,
+    author="Sebastian Sanchez Perez-Moreno",
+    author_email="s.sanchezperezmoreno@tudelft.nl",
+    url='https://github.com/sebasanper/WINDOW_openMDAO',
+    packages=[
+        'WINDOW_openMDAO',
+        'WINDOW_openMDAO.fast_workflow_irregular',
+        'WINDOW_openMDAO.input_params',
+        'WINDOW_openMDAO.regular_parameterised',
+        'WINDOW_openMDAO.workflow_irregular',
+        'WINDOW_openMDAO.workflow_regular',
+        'WINDOW_openMDAO.src',
+        'WINDOW_openMDAO.Costs',
+        'WINDOW_openMDAO.ElectricalCollection',
+        'WINDOW_openMDAO.Finance',
+        'WINDOW_openMDAO.OandM',
+        'WINDOW_openMDAO.SupportStructure',
+        'WINDOW_openMDAO.Turbine',
+        'WINDOW_openMDAO.WakeModel',
+        'WINDOW_openMDAO.WaterDepth',
     ]
 )
